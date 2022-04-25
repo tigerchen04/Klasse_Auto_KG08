cb1c634dcf9ff62756fe4c14f14a61e5c7057e21
tigerchen04 <104207588+tigerchen04@users.noreply.github.com>
2022-04-25 14:44:56 +0200

    cpp-Datei

---
+++ Klasse_Auto.cpp
@@ -0,0 +1,14 @@
+#include"Klasse_Auto.h"
+#include <iostream>
+using namespace std;
+
+Klasse_Auto::Klasse_Auto()
+	{	
+	radzahl = 4;
+	farbe = "rot";
+	ps = 65;
+	anzahl_sitze = 5;
+	spritverbrauch = 6;
+	tankinhalt = 43;
+	tankgroesse = 43;
+}
