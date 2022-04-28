00cf623268c05a49f9a85aa13f868fc06e470c61
PaulFrankowski <94003153+PaulFrankowski@users.noreply.github.com>
2022-04-28 16:31:38 +0200

    Objekt mehrmals erstellen (Stack, Datensegment, Heap)

--- Testprogramm.cpp
+++ Testprogramm.cpp
@@ -8,8 +8,16 @@ int main()
 Klasse_Auto Auto_1 = Klasse_Auto();
 
 //Objekt erstellen durch allgemeinen Konstruktor:
-Klasse_Auto Auto_1 = Klasse_Auto()
-Klasse_Auto{
-	Klasse_Auto();
+Klasse_Auto Auto_2 = Klasse_Auto(4,"rot",65,5,6.f,43.f,43.f );
+
+//Kopierkonstruktor:
+Klasse_Auto Auto_3 = Klasse_Auto(Auto_2);
 	
+//Objekt erstellen auf dem Stack:
+Klasse_Auto Auto_1 = Klasse_Auto();
+//Objekt erstellen im Datensegment:
+static Klasse_Auto Auto_2 = Klasse_Auto();
+//Objekt erstellen auf dem Heap:
+Klasse_Auto* Auto_3 = new Klasse_Auto();
+delete Auto_3;
 };
